a410d9620804f936a7bfde71ed4ebea6b4d97726
danielmtro <Daniel.Monteiro2205@gmail.com>
2024-09-18 17:10:42 +1000

    progressed general neutral state logic

--- src/vroom/include/constants.hpp
+++ src/vroom/include/constants.hpp
@@ -1,6 +1,10 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+#include <math.h>
+
 constexpr int STANDARD_BUFFER_SIZE = 10;
+constexpr double DEG2RAD (M_PI / 180.0);
+constexpr double RAD2DEG (180.0 / M_PI);
 
 #endif
--- src/vroom/src/fsm.cpp
+++ src/vroom/src/fsm.cpp
@@ -8,7 +8,7 @@ FSM CLASS IMPLEMENTATION
 
 using namespace std::chrono_literals;
 
-FSM::FSM(): Node("fsm_node"), current_state_(0)
+FSM::FSM(): Node("fsm_node"), current_state_(GET_TB3_DIRECTION)
 {
     // Create instances of Lidar, Odom, and Cam nodes
     lidar_node_ = std::make_shared<Lidar>();
@@ -54,13 +54,68 @@ void FSM::update_state()
    {
 
         case LOCATE_WALL:
-            
+            /*
+            STILL HAVE TO DO THIS
+            */
+            break;
+
+        case TURN_TO_WALL:
+            /*
+            STILL HAVE TO IMPLEMENT THIS
+            */
             break;
 
         case GET_TB3_DIRECTION:
 
-            //TODO ADD IN THIS BASED ON WHATEVER ADAM ENDS UP DOING WITH THE LIDAR
-            scan_reference;
+            // extract the lidar data
+            scan_data_ = lidar_node_.get_scan_data();
+    
+            if (scan_data_[CENTER] > CHECK_FORWARD_DIST)
+            {
+                if (scan_data_[LEFT] < CHECK_SIDE_DIST)
+                {
+                    prev_robot_pose_ = odom_node_.get_robot_pose();
+                    prev_scan_data_ = scan_data_;
+                    current_state_ = TB3_RIGHT_TURN;
+                }
+                else if (scan_data_[RIGHT] < CHECK_SIDE_DIST)
+                {
+                    prev_robot_pose_ = odom_node_.get_robot_pose();
+                    prev_scan_data_ = scan_data_;
+                    current_state_ = TB3_LEFT_TURN;
+                }
+                else if (scan_data_[HARD_LEFT] > (NO_WALL_DIST) && prev_scan_data_[HARD_LEFT] <= NO_WALL_DIST)
+                {
+                    prev_robot_pose_ = robot_pose_;
+                    prev_scan_data_ = scan_data_;
+                    turtlebot3_state_num = TB3_LEFT_90;
+                }
+                // if the wall on the left is getting too further away, turn towards it
+                else if (scan_data_[LEFT] > prev_scan_data_[LEFT] && scan_data_[LEFT] >  (1.3 * CHECK_SIDE_DIST) && (scan_data_[LEFT] < 2* CHECK_SIDE_DIST)){
+                prev_robot_pose_ = robot_pose_;
+                prev_scan_data_ = scan_data_;
+                turtlebot3_state_num = TB3_LEFT_TURN;
+                }
+                //if a left hand corner is approaching
+                else if (scan_data_[LEFT] > 1.5 * CHECK_SIDE_DIST && prev_scan_data_[LEFT] > 1.5 * CHECK_SIDE_DIST){
+                    prev_robot_pose_ = robot_pose_;
+                    prev_scan_data_ = scan_data_;
+                    turtlebot3_state_num = TB3_SLOW_FORWARD;
+                }
+                // if we are not close to any walls then keep driving forward
+                else {
+                    prev_scan_data_ = scan_data_;
+                    turtlebot3_state_num = TB3_DRIVE_FORWARD;
+                }
+            }
+
+            // if there is something in front of the robot then turn right
+            if (scan_data_[CENTER] < CHECK_FORWARD_DIST) {
+                prev_scan_data_ = scan_data_;
+                prev_robot_pose_ = robot_pose_;
+                turtlebot3_state_num = TB3_RIGHT_TURN;
+            }
+
             break;
         
         case TB3_DRIVE_FOWARD:
