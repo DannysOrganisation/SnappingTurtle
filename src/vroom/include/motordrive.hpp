3e5624c64d95df9907fad126e0339224c7a00fde
AdamRiesel <riesel.adam@gmail.com>
2024-09-18 12:47:56 +1000

    state output is finished and ready for review

--- src/vroom/include/motordrive.hpp
+++ src/vroom/include/motordrive.hpp
@@ -18,30 +18,75 @@ it should publish to the /cmd_vel topic
 #include <vector>
 #include <sensor_msgs/msg/laser_scan.hpp>
 #include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+
 
 #include "enum.h"
 #include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/int32.hpp"
+
+#define LINEAR_VELOCITY  0.3
+#define ANGULAR_VELOCITY 0.2
 
 class Motordrive : public rclcpp::Node{
     public:
-        CMotordrive();
-        ~CMotordrive();
-
+        Motordrive();
+        ~Motordrive();
     private:
-        void fsm_callback(const std_msg::msg::Int32 msg);
+        
+        /**
+        * @brief this function is called every time a message is received from
+        * /state topic and it calls the appropriate function for each state
+        *
+        * @param msg is an 32 bit integer which represents the current state 
+        */
+        void state_callback(const std_msgs::msg::Int32 msg);
 
+        /**
+        *@brief this function publishes to /cmd_vel topic with the desired 
+        * velocity for the turtlebot
+        *
+        * @param linear is the linear velocity in m/s
+        * @param angular is the angular velocity in rad/s 
+         */
+        void update_cmd_vel(double linear, double angular);
+
+        /**
+        * @brief this function has the robot turn left on the spot
+         */
         void turn_left();
+
+        /**
+        * @brief this function has the robot turn right on the spot
+         */
         void turn_right();
+
+        /**
+        * @brief this function has the robot turn left in an arc
+         */
         void turn_hard_left();
+
+        /**
+        * @brief this function has the robot turn right in an arc
+         */
         void turn_hard_right();
+
+        /**
+        * @brief this function has the robot drive straight at a desired speed
+         */
         void drive_forward();
+
+        /**
+        * @brief this function has the robot drive straight at a slow speed
+         */
         void slow_forward();
 
+        //memory of the current state
         int current_state_;
 
-        rclcpp::Subscription<std_msg::msg::Int32>::SharedPtr motore_driver_sub_;
-
-
-}
+        //publish to cmd_vel and subscribe to state
+        rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
+        rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr motor_driver_sub_;
+};
 
 #endif
