86b520c18a94d297780da32af4a07ed831957c0b
danielmtro <Daniel.Monteiro2205@gmail.com>
2024-09-20 14:26:21 +1000

    added fsm to main

--- src/vroom/src/main.cpp
+++ src/vroom/src/main.cpp
@@ -1,8 +1,12 @@
-#include "lidar.hpp"
+#include "fsm.hpp"
 
 int main(int argc, char ** argv)
 {
     rclcpp::init(argc, argv);
+
+    // Create the FSM state transition node (NOT FOR TESTING)
+    rclcpp::spin(std::make_shared<FSM>());
+
     rclcpp::shutdown();
 
   return 0;
