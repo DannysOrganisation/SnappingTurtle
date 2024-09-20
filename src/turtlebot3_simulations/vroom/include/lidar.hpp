0aec4c39c3643f762a759281493fe04769040423
Will Ridley-Smith <wrid7227@uni.sydney.edu.au>
2024-09-20 14:15:26 +1000

    Moving vroom directory into simulations folder

---
+++ src/turtlebot3_simulations/vroom/include/lidar.hpp
@@ -0,0 +1,23 @@
+/*
+
+TODO
+
+Interface should read from /scan
+
+Should store this in a member called distances
+
+it should publish 4 doubles that correspond to the distances in order as
+
+CENTER LEFT RIGHT HARD_RIGHT
+0       30  330     90
+
+Check ENUM for more details
+*/
+
+#ifndef LIDAR_HPP
+#define LIDAR_HPP
+
+#include "enum.h"
+
+
+#endif
---
+++ src/turtlebot3_simulations/vroom/include/motordrive.hpp
@@ -0,0 +1,12 @@
+/*
+
+TODO 
+
+This should control the drive based on the current state.
+
+It should subscribe to the /state topic
+it should publish to the /cmd_vel topic
+*/
+
+
+#include "enum.h"
