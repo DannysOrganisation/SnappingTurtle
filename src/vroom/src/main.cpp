47539191b84703fd9443929d3913a531aa81e4d9
James-h-1969 <jameshocking542@gmail.com>
2024-09-20 20:40:28 +1000

    removing testing from main

--- src/vroom/src/main.cpp
+++ src/vroom/src/main.cpp
@@ -5,12 +5,6 @@ int main(int argc, char ** argv)
 {
     rclcpp::init(argc, argv);
 
-    // Create a node
-    auto node = std::make_shared<CameraReader>();
-
-    // Spin the node to process callbacks
-    rclcpp::spin(node);
-
     rclcpp::shutdown();
     return 0;
 }
