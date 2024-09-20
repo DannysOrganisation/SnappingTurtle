7adfb353f703a1121ace1a9a7ebe9119c01c8359
danielmtro <Daniel.Monteiro2205@gmail.com>
2024-09-20 13:48:17 +1000

    UPDATE: fixed magic numbers in lidar.hpp

--- src/vroom/include/enum.h
+++ src/vroom/include/enum.h
@@ -10,7 +10,17 @@ enum Directions {
     HARD_RIGHT
 };
 
+enum Views {
+    CENTER_ANGLE = 0,
+    LEFT_ANGLE = 30,
+    RIGHT_ANGLE = 330,
+    HARD_LEFT_ANGLE = 90,
+    HARD_RIGHT_ANGLE = 270
+}
+
+
 enum States {
+<<<<<<< HEAD
  LOCATE_WALL = 0,
  TURN_TO_WALL,
  GET_TB3_DIRECTION,
@@ -19,8 +29,15 @@ enum States {
  TB3_LEFT_TURN,
  TB3_LEFT_90,
  TB3_SLOW_FORWARD
+=======
+    LOCATE_WALL = 0,
+    GET_TB3_DIRECTION,
+    TB3_DRIVE_FORWARD,
+    TB3_RIGHT_TURN,
+    TB3_LEFT_TURN,
+    TB3_LEFT_90,
+    TB3_SLOW_FORWARD
+>>>>>>> fddb228 (UPDATE: fixed magic numbers in lidar.hpp)
 };
-
-
 
 #endif
